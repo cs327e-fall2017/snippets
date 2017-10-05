4b9abd31af14c43d9a75f59f582d89aea0cec5dd
Shirley Cohen <shirley.cohen@rackspace.com>
2017-10-05 10:42:15 -0500

    Add files via upload

--- copy_principals_from_split_files.sql
+++ copy_principals_from_split_files.sql
@@ -1,37 +1,25 @@
 \copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xaa' (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xab' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xab' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xac' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xac' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xad' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xad' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xae' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xae' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xaf' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xaf' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xag' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xag' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xah' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xah' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xai' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xai' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xaj' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xaj' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xak' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xak' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xal' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xal' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from 
-'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xam' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_principals/xam' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
