ceb6e745a3bb5bab41248ea0a7ed11df86b64dda
Shirley Cohen <shirley.cohen@rackspace.com>
2017-10-05 12:56:13 -0500

    Add files via upload

---
+++ copy_stars_from_split_files.sql
@@ -0,0 +1,13 @@
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xaa' (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xab' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xac' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xad' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xae' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xaf' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy stars from 'C:/utcs_work/cs327e_fall_2017/datasets/imdb/pg/split_stars/xag' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
